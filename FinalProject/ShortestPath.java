6089842e9fb8adbce77444b536da2c46a049e35e
jackbarron <jackbarron@localhost>
2022-04-03 19:19:21 +0100

    start of shortest path + graphing it.

--- FinalProject/ShortestPath.java
+++ FinalProject/ShortestPath.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //Finding shortest paths between 2 bus stops (as input by the user), returning the list of stops
 //en route as well as the associated “cost”.
 //Stops are listed in stops.txt and connections (edges) between them come from stop_times.txt and
@@ -13,7 +16,49 @@
 //transfers.txt with transfer type 0 (which is immediate transfer possible), and for transfer type 2 the
 //cost is the minimum transfer time divided by 100.
 
+//adjacency matrix representation of a graph, undirected/directed. This will create a graph by 
+//parsing through the data files and reading it into a graph.
+
+
 public class ShortestPath {
+	//input file names
+	private String stop_timesTxt; 
+	private String transfersTxt;
+	//adjacency matrix for graph
+	private double adjMatrix[][] = new double [10000][10000]; //dimension expressions needed, will start with 10,000
+	
+	//want to print the shortest path graph, need a public main, then publicly define the method , then private create graph
+	//want the main to just return the shortest path
+	public static void main(String[] args) throws FileNotFoundException{
+		
+		showShortestPath(); //define this method
+	}
+	
+	public static void showShortestPath() throws FileNotFoundException{
+		ShortestPath map = new ShortestPath("stop_timesTxt", "transfersTxt");
+		
+		try
+		
+		{
+			map.createGraph(); //need to define this
+		}
+		
+		//take user input
+		Scanner userInput = new Scanner (System.in);
+		System.out.print("Please enter starting bus stop location.");
+		int startPlace = userInput.nextInt();
+		
+		System.out.print("Please enter your final destinations bus stop.");
+		int finishPlace = userInput.nextInt();
+		
+		//can then print shortest distance between the two user inputted locations, first need to create this.
+		
+	
+			
+		
+	}
+	
+	
 
 	
 }
