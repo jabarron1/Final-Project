33081481b4450ad33275d259c16c8a80bf83565c
jackbarron <jackbarron@localhost>
2022-04-07 22:11:41 +0100

    Front Interface.

--- FinalProject/FrontInterface.java
+++ FinalProject/FrontInterface.java
@@ -1,4 +1,11 @@
+//Provide front interface enabling selection between the above features or an option to exit
+//the programme, and enabling required user input. It does not matter if this is command-line
+//or graphical UI, as long as functionality/error checking is provided.
+//You are required to provide error checking and show appropriate messages in the case of erroneous
+//inputs – eg bus stop doesn’t exist, wrong format for time for bus stop (eg letters instead of
+//numbers), no route possible etc.
 
+//need to choose between command line or graphical UI.
 public class FrontInterface {
 
 }
