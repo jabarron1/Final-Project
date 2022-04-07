92d7772edc29937438024849ba70d39a237f3174
jackbarron <jackbarron@localhost>
2022-04-07 20:15:07 +0100

    onto user inputted arrival times.

--- FinalProject/SearchForAllTrips.java
+++ FinalProject/SearchForAllTrips.java
@@ -1,6 +1,11 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.ParseException;
+import java.util.Scanner;
 
 //Searching for all trips with a given arrival time, returning full details of all trips matching the
 //criteria (zero, one or more), sorted by trip id
@@ -34,7 +39,55 @@ public class SearchForAllTrips {
 		String dropoffType = "";
 		String shapeDistTravelled = "";
 		
+		try { //same as SearchForBusStopTST
+			FileWriter a = new FileWriter(tempStopsFile, true);
+			BufferedWriter b = new BufferedWriter(a);
+			PrintWriter c = new PrintWriter(b);
+			Scanner scanner = new Scanner(new File(stopsFile));
+			scanner.useDelimiter("[,\n]");
+			scanner.nextLine();
+			
+			
+			while(scanner.hasNext()) {
+				tripID = scanner.next();
+				arrivalTime = scanner.next();
+				departureTime = scanner.next();
+				stopID = scanner.next();
+				stopSequence = scanner.next();
+				stopHeadSign = scanner.next();
+				pickupType = scanner.next();
+				dropoffType = scanner.next();
+				shapeDistTravelled = scanner.next();
+				
+				//if statement
+				//arrival time is less than timeInvalid
+				if(arrivalTime.compareTo(validTime) < 0) {
+					c.println(tripID + "" + arrivalTime + "" + departureTime + "" + stopID + "" + stopSequence + "" +
+				            stopHeadSign + "" + pickupType + "" + dropoffType + "" + shapeDistTravelled);
+				}
+				else {
+					c.println(tripID + "" + arrivalTime + "" + departureTime + "" + stopID + "" + stopSequence + "" +
+				            stopHeadSign + "" + pickupType + "" + dropoffType + "" + shapeDistTravelled);
+				}
+			}
+			scanner.close();
+			c.flush();
+			c.close();
+			originalFile.delete();
+			//need snapshot of file
+			File snapDump = new File(stopsFile);
+			removedFile.renameTo(snapDump);
+			
+		}//catch
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
 }
+
+//now need to be able to take user inputted arrival time sorted by trip id, removing invalid times.
